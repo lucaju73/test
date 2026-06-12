0eed992c2fd7c4717e4f025b4f8472851350f780
lucaju73 <64251183+lucaju73@users.noreply.github.com>
2026-06-12 15:40:16 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
+import folium
+from streamlit_folium import st_folium
 
-st.title("plotly 테스트")
+st.title("folium 지도 테스트")
+st.success("folium, streamlit-folium 정상 설치")
 
-df = pd.DataFrame({
-    "x": [1,2,3],
-    "y": [4,5,6]
-})
+# 서울시청 중심 지도
+m = folium.Map(
+    location=[37.5665, 126.9780],
+    zoom_start=11,
+    tiles="cartodbpositron"
+)
 
-fig = px.scatter(df, x="x", y="y")
+folium.Marker(
+    [37.5665, 126.9780],
+    popup="서울시청",
+    tooltip="서울시청"
+).add_to(m)
 
-st.plotly_chart(fig)
+st_folium(m, width=700, height=500)
