75d3b1e86b699cf0b6d25a83314823123376782f
lucaju73 <64251183+lucaju73@users.noreply.github.com>
2026-06-12 15:36:00 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-st.title("pandas 테스트")
-st.success("pandas 정상 설치")
+st.title("plotly 테스트")
+
+df = pd.DataFrame({
+    "x": [1,2,3],
+    "y": [4,5,6]
+})
+
+fig = px.scatter(df, x="x", y="y")
+
+st.plotly_chart(fig)
